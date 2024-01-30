d11da894e3cf4d81d8066b01bdd741f36aa514e6
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-01-30 16:41:27 +0600

    while loop added

---
+++ for-of.js
@@ -0,0 +1,15 @@
+const numbers = [10, 11, 12, 13, 14, 15];
+
+console.log(numbers);
+
+for (const num of numbers) {
+  console.log(num);
+}
+
+const fruits = ['mango', 'banana', 'apple', 'jackfruit', 'watermelon'];
+
+console.log(fruits);
+
+for (const fruit of fruits) {
+  console.log(fruit);
+}
---
+++ while.js
@@ -0,0 +1,14 @@
+const number = 12;
+
+if (number < 10) {
+  console.log("small number");
+} else {
+  console.log('large number');
+}
+
+// While Loop
+let num = 0;
+while (num < 5) {
+  console.log('Looping', num);
+  num++;
+}
